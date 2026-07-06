a360524af5de2e35daa58b479217f27d3a7e07f7
islammdsohan603 <islammdsohan603@gmail.com>
2026-07-06 18:33:50 +0600

    otp send email

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 
-
 const app = express();
-
 app.use(cors());
-
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
@@ -23,7 +20,6 @@ const client = new MongoClient(process.env.MONGODB_URL, {
 
 async function run() {
   try {
-
     await client.connect();
     console.log("🎯 Successfully connected to MongoDB Atlas!");
 
@@ -39,8 +35,6 @@ async function run() {
       }
     });
 
-    // get all books api
-
     app.get("/api/all-books", async (req, res) => {
       try {
         const result = await booksCollection.find().toArray();
@@ -50,29 +44,24 @@ async function run() {
       }
     });
 
-    // get single data api
-
     app.get("/api/details/:id", async (req, res) => {
       try {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) };
         const result = await booksCollection.findOne(query);
         if (!result) {
-          return res.status(404).send({ message: "Books not founds" })
+          return res.status(404).send({ message: "Books not found" });
         }
-
-        res.send(result)
-
+        res.send(result);
       } catch (err) {
         res.status(500).send({ message: "Error fetching books", error: err });
       }
-    })
+    });
 
   } catch (error) {
     console.error("❌ MongoDB Connection Error:", error.message);
   }
 }
-
 
 run();
 
